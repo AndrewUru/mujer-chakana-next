aa28fd394d4772f1ae98570159a5adc8b9901d59
Andres Tobio <92232669+AndrewUru@users.noreply.github.com>
2025-04-23 16:39:58 +0200

    mejorar configuración de Next.js: ajustar comentarios y formateo en next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,20 +3,21 @@ import type { NextConfig } from "next";
 // Importa el wrapper de PWA
 import withPWA from "next-pwa";
 
-// Config principal de Next.js
 const nextConfig: NextConfig = {
   reactStrictMode: true,
 
-  // ⚠️ Corrige el error de tipo con `serverActions`
-  // En lugar de true, pasamos un objeto vacío (si no vas a usar nada aún)
   experimental: {
     serverActions: {},
+  },
+
+  images: {
+    domains: ["onlnbinfmtfbawocixf.supabase.co"], // Reemplaza con tu dominio Supabase si es diferente
   },
 };
 
 // Exporta el config envuelto con PWA
 export default withPWA({
-  dest: "public", // Dónde se generará el sw.js
-  register: true, // Registrar el SW automáticamente
-  skipWaiting: true, // Reemplazar inmediatamente el SW anterior
+  dest: "public",
+  register: true,
+  skipWaiting: true,
 })(nextConfig);
