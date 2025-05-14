606dc843becb468e740453c037d5ad467edbc2d6
Andres Tobio <92232669+AndrewUru@users.noreply.github.com>
2025-05-14 20:05:26 +0200

    feat: eliminar elementos de fondo y ajustar estilos en la página de inicio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,41 +7,12 @@ export default function HomePage() {
   const router = useRouter();
 
   return (
-    <main className="min-h-screen bg-[url('/bg-chakana.png')] bg-cover bg-center rounded-2xl text-pink-900 px-4 py-2 sm:py-2 relative pb-30">
-      <div className="max-w-4xl mx-auto rounded-2xl space-y-12">
+    <main className="min-h-screen bg-[url('/bg-chakana.png')] bg-cover bg-center text-pink-900 px-4 py-2 sm:py-2 ">
+      <div className="max-w-4xl mx-auto rounded-2xl space-y-12 pb-20">
         {/* Logo y bienvenida - Versión Mejorada */}
         <div className="relative overflow-hidden">
-          {/* Luna atravesando el fondo oscuro */}
-          <div className="absolute inset-0 bg-transparent z-[-1] overflow-hidden">
-            <div className="absolute top-1/2 -translate-y-1/2 w-20 h-20 animate-[moon-pass_10s_linear_infinite]">
-              <Image
-                src="/luna.png"
-                alt="Luna fondo"
-                width={566}
-                height={566}
-                className="object-contain opacity-30 blur-sm"
-              />
-            </div>
-          </div>
           {/* Fondo blur general */}
           <div className="absolute inset-0 bg-white/50 backdrop-blur-4xl z-0" />
-
-          {/* Fondos decorativos con luces suaves */}
-          <div className="absolute -top-24 -left-24 w-96 h-96 bg-rose-200 opacity-30 rounded-full blur-[120px] z-0" />
-          <div className="absolute top-1/2 -right-20 w-72 h-72 bg-rose-300 opacity-20 rounded-full blur-[100px] z-0" />
-
-          {/* Luna atravesando el fondo oscuro */}
-          <div className="absolute inset-0 bg-transparent z-[-1] blur-md overflow-hidden">
-            <div className="absolute top-1/2 -translate-y-1/2 w-20 h-20 animate-[moon-pass_10s_linear_infinite]">
-              <Image
-                src="/luna.png"
-                alt="Luna fondo"
-                width={566}
-                height={566}
-                className="object-contain opacity-60 blur-sm"
-              />
-            </div>
-          </div>
 
           {/* Contenido principal */}
           <div className="relative flex flex-col items-center text-center space-y-8 py-8 px-6 rounded-2xl max-w-4xl mx-auto z-10">
@@ -52,12 +23,12 @@ export default function HomePage() {
                 alt="Logo Mujer Chakana"
                 fill
                 priority
-                className="object-contain p-4 rounded-full hover:scale-[1.03] transition-transform duration-300"
+                className="object-contain rounded-full hover:scale-[1.03] transition-transform duration-300"
               />
             </div>
 
             {/* Título con imagen de luna */}
-            <h1 className="text-2xl sm:text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-rose-700 to-rose-900 flex items-center gap-4 drop-shadow-lg">
+            <h1 className="text-2xl sm:text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-rose-700 to-rose-900 flex items-center gap-4 drop-shadow-lg rounded-2xl">
               Mujer Chakana
             </h1>
 
